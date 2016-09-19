d3fad1e0849c3d4e2ff2c9aa2e83cac9f1a54fbf
Jordan Chong <jordanchong23@gmail.com>
2016-09-19 12:22:22 -0700

    update default route

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,6 +2,10 @@ var sendGridHelpers = require('../emailHelpers/sendGridHelpers.js');
 let mailGunHelpers = require('../emailHelpers/mailGunHelpers.js');
 
 module.exports = function(app, io) {
+  // API Routes
+  app.post('/', (req, res, next) => {
+    res.send('Simple Email Service');
+  });
   // socket events 
   io.on('connection', function(socket) {
     console.log('Connection ESTABLISHED!');
